61c0d8132c1f77f196bab1bce66cfe60571d3bae
Matteo <55857169+mattepica@users.noreply.github.com>
2022-01-02 21:16:20 +0100

    Delete backupCasalino.sh

--- backupCasalino.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-FILE=$(</opt/minecraftCasalino/backups/lastFile.dat)
-
-rclone copy /opt/minecraftCasalino/backups/$FILE mega:backupCasalino
-
-rclone delete --mega-hard-delete  mega:backupCasalino --min-age 8d -v
